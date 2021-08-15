deca535e353eb95894fda01679aab5912aa8265e
Tiger14n <nasser11r1@gmail.com>
2021-08-15 14:17:08 +0300

    Update lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -1,6 +1,7 @@
 const { MessageEmbed } = require("discord.js")
 const { COLOR } = require("../config.json");
 const solenolyrics= require("solenolyrics"); 
+const Client  = new (require("genius-lyrics")).Client("ZD_lLHBwRlRRfQvVLAnHKHksDHQv9W1wm1ZAByPaYo1o2NuAw6v9USBUI1vEssjq");
 module.exports = {
   name: "lyrics",
   description: "Get lyrics of song :v",
@@ -9,6 +10,8 @@ module.exports = {
     .setDescription("Looking For Lyrics ...")
     .setColor("YELLOW")
     const msg = await message.channel.send(embed)
+   
+
     //nothing is playing and didnt write a song name
     if(!args.length) {
 
@@ -22,7 +25,6 @@ module.exports = {
       }
       // song is playing
       songtitle = serverQueue.songs[0].title
-        console.log(songtitle);
          getlyricsfromtitle();
 
           
@@ -33,42 +35,93 @@ module.exports = {
 
     //  return message.channel.send("Please Give The Song Name")
     } else if (args.length) {
-    
-   
+    var ab = 0;
+      async function showinglyricsargs() {
      try {
-          const songs = await Genius.tracks.search(args.join(" "));
-          const lyrics = await songs[0].lyrics();
+
+      const searches = await Client.songs.search(args.join(" "));
+      const firstSong = searches[0];
+      const lyrics = await firstSong.lyrics();
+      const songData = await firstSong.fetch();
+      songtitle = songData.fullTitle
+      thumbnaillink = songData.thumbnail
+
+      
+
+         // const songs = await Genius.tracks.search(args.join(" "));
+        //  const lyrics = await songs[0].lyrics();
           
-           if (lyrics.length > 4095) {
-             msg.delete()
-        return message.channel.send('Lyrics are too long to be returned as embed');
-     }
-      if (lyrics.length < 2048) {
-        const lyricsEmbed = new MessageEmbed()
-          .setColor(COLOR)
-          .setDescription(lyrics.trim());
-        return msg.edit(lyricsEmbed);
-      } else {
-        // lyrics.length > 2048
-        const firstLyricsEmbed = new MessageEmbed()
-          .setColor(COLOR)
-          .setDescription(lyrics.slice(0, 2048));
-        const secondLyricsEmbed = new MessageEmbed()
-          .setColor(COLOR)
-          .setDescription(lyrics.slice(2048, lyrics.length));
-        msg.edit(firstLyricsEmbed);
-        message.channel.send(secondLyricsEmbed);
-        return;
-      }
+        if (lyrics.length > 4095) {
+             
+          
+          const firstLyricsEmbed = new MessageEmbed()
+          .setAuthor("Lyrics", client.user.displayAvatarURL())
+          .setThumbnail(thumbnaillink)
+
+        .setColor("BLACK")
+        .setDescription('\n' +songtitle+"```"+lyrics.slice(0, 2048)+"```");
+
+      const secondLyricsEmbed = new MessageEmbed()
+        .setColor("BLACK")
+        .setDescription("```"+lyrics.slice(2048, 4095)+"```");
+      msg.edit(firstLyricsEmbed);
+      message.channel.send(secondLyricsEmbed);
+
+
+      return;
+   }
+    if (lyrics.length < 2048) {
+      const lyricsEmbed = new MessageEmbed()
+      .setAuthor("Lyrics", client.user.displayAvatarURL())
+      .setThumbnail(thumbnaillink)
+        .setColor("BLACK")
+        .setDescription('\n' +songtitle+"```"+lyrics.trim()+"```");
+      return msg.edit(lyricsEmbed);
+  
+  
+   
+    } if (lyrics.length < 4095 && lyrics.length > 2048 ) {
+      // lyrics.length > 2048
+      const firstLyricsEmbed = new MessageEmbed()
+      .setAuthor("Lyrics", client.user.displayAvatarURL())
+      .setThumbnail(thumbnaillink)
+
+        .setColor("BLACK")
+        .setDescription("\n" +songtitle+"```"+lyrics.slice(0, 2048)+"```");
+      const secondLyricsEmbed = new MessageEmbed()
+        .setColor("BLACK")
+        .setDescription("```"+lyrics.slice(2048, lyrics.length)+"```");
+      msg.edit(firstLyricsEmbed);
+      message.channel.send(secondLyricsEmbed);
+      return;
+    
+  }
       
        
      } catch(e) {
        
-       embed.setDescription("Got err : " + e)
-       msg.edit(embed)
-          console.log(e);
+    
+ 
+          if (ab == 10){
+            embed.setColor("RED")
+              embed.setDescription("LYRICS NOT FOUND..")
+                  msg.edit(embed);
+                console.log();
+                return;
+             
+            
+             } else ab++;
+             console.log(ab);  showinglyricsargs();
+             console.log();
+
      }
     
+
+
+
+    
+    }
+    showinglyricsargs();
     }
 
 
@@ -107,8 +160,13 @@ let part2 = songtitle.substring(
      try {
 		 
      try {
-  var lyrics = await solenolyrics.requestLyricsFor(input)
-		  	 }
+       
+  var songinfo = await getSonginfo(input)
+
+ 
+
+      
+     }  
 catch(e) {
 	  embed.setDescription("Got err : " + e)
        msg.edit(embed)
@@ -118,35 +176,55 @@ catch(e) {
 	
 	 function waitForElement(){
 	
-    if (lyrics !== null){
-		
+    if (songinfo !== null){
+		  var lyrics =  songinfo.lyrics
+  var thumbnaillinkx =  songinfo.thumbnail.genius
+  var songtitlex =  songinfo.title
            if (lyrics.length > 4095) {
-             msg.delete()
+             
        
-		embed.setDescription("Lyrics are too long to be returned as embed ")
-       msg.edit(embed);
-     }
-      if (lyrics.length < 2048) {
-        const lyricsEmbed = new MessageEmbed()
-          .setColor("GREEN")
-          .setDescription(lyrics.trim());
-        return msg.edit(lyricsEmbed);
-		
-	  
-	   
-      } if (lyrics.length < 4095 && lyrics.length > 2048 ) {
-        // lyrics.length > 2048
-        const firstLyricsEmbed = new MessageEmbed()
-          .setColor("GREEN")
-          .setDescription(lyrics.slice(0, 2048));
-        const secondLyricsEmbed = new MessageEmbed()
-          .setColor("GREEN")
-          .setDescription(lyrics.slice(2048, lyrics.length));
-        msg.edit(firstLyricsEmbed);
-        message.channel.send(secondLyricsEmbed);
-        return;
-      
-	  }
+            const firstLyricsEmbed = new MessageEmbed()
+          .setAuthor("Lyrics", client.user.displayAvatarURL())
+          .setThumbnail(thumbnaillinkx)
+
+        .setColor("BLACK")
+        .setDescription('\n' +songtitlex+"```"+lyrics.slice(0, 2048)+"```");
+
+      const secondLyricsEmbed = new MessageEmbed()
+        .setColor("BLACK")
+        .setDescription("```"+lyrics.slice(2048, 4095)+"```");
+      msg.edit(firstLyricsEmbed);
+      message.channel.send(secondLyricsEmbed);
+
+
+      return;
+   }
+    if (lyrics.length < 2048) {
+      const lyricsEmbed = new MessageEmbed()
+      .setAuthor("Lyrics", client.user.displayAvatarURL())
+      .setThumbnail(thumbnaillinkx)
+        .setColor("BLACK")
+        .setDescription('\n' +songtitlex+"```"+lyrics.trim()+"```");
+      return msg.edit(lyricsEmbed);
+  
+  
+   
+    } if (lyrics.length < 4095 && lyrics.length > 2048 ) {
+      // lyrics.length > 2048
+      const firstLyricsEmbed = new MessageEmbed()
+      .setAuthor("Lyrics", client.user.displayAvatarURL())
+      .setThumbnail(thumbnaillinkx)
+
+        .setColor("BLACK")
+        .setDescription("\n" +songtitlex+"```"+lyrics.slice(0, 2048)+"```");
+      const secondLyricsEmbed = new MessageEmbed()
+        .setColor("BLACK")
+        .setDescription("```"+lyrics.slice(2048, lyrics.length)+"```");
+      msg.edit(firstLyricsEmbed);
+      message.channel.send(secondLyricsEmbed);
+      return;
+    
+  }
 	 
 	 }
 	 else {
@@ -195,4 +273,13 @@ showinglyrics();
 
   }
   
+}
+const fetch = require('node-fetch')
+async function getSonginfo (title) {
+  if (!title) throw new TypeError('Input value was undefined!');
+  return new Promise(async function(resolve, reject) {
+  var response = await fetch(`https://some-random-api.ml/lyrics/?title=${title}`)
+  response = await response.json()
+  resolve(response)
+})
 }
